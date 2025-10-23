0a8cf28a7d139295a7f23f309fd5904e65db7d3f
pullsina <sovya22@gmail.com>
2025-10-23 16:52:07 +0200

    Added mwthods Edit&Copy to Recipe class

--- CookMasterApp/Models/Recipe.cs
+++ CookMasterApp/Models/Recipe.cs
@@ -10,15 +10,40 @@ namespace CookMasterApp.Models
     {
         //properties
         public string Title { get; set; }
-        public string Ingrediants { get; set; }
-        public string Instruction { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
         public string Category { get; set; }
         public DateTime Date { get; set; }
         public User CreatedBy { get; set; }
 
+        //Constructor
+        public Recipe(string title, string ingredients, string instructions, string category, User createdBy)
+        {
+            Title = title;
+            Ingredients = ingredients;
+            Instructions = instructions;
+            Category = category;
+            CreatedBy = createdBy;
+            Date = DateTime.Now;
+        }
+
 
         //Methods 
         //EditRecipe()
+        public void EditRecipe(string newTitle, string newIngredients, string newInstructions, string newCategory)
+        {
+            Title = newTitle;
+            Ingredients = newIngredients;
+            Instructions = newInstructions;
+            Category = newCategory;
+            Date = DateTime.Now;
+        }
+
         //CopyRecipy ()
+        public Recipe CopyRecipy ()
+        {
+            return new Recipe(Title, Ingredients, Instructions, Category, CreatedBy);
+        }
+
     }
 }
